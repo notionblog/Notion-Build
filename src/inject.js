c93415c4c9df7b7b39fb3c3831b5a7b9a4691ee4
yudaxx <yudax42@gmail.com>
2021-05-21 13:43:38 +0100

    fix button style

--- src/inject.js
+++ src/inject.js
@@ -6,17 +6,18 @@ chrome.storage.sync.get(['link', 'badge'], function (result) {
     const buttonDiv = document.createElement('DIV')
     buttonDiv.setAttribute("id", "trigger_build")
     buttonDiv.innerHTML = `
-                <button id="buildButton" style="color: white;
+                <button id="buildButton" style="
+                color: white;
                 line-height: 1.2;
                 background: rgb(0, 150, 109);
                 padding-left: 8px;
                 padding-right: 8px;
                 font-size: 14px;
                 font-weight: 500;
-                height: 26px;
+                height: 44px;
                 border: none;
-                margin-right: 12px;
-                margin-left: 3px;
+                margin-right: 7px;
+                margin-left: 11px;
                 border-radius: 2px;" type="submit">🚀 Publish changes</button>
             `
     let imgbadge = document.createElement('img');
