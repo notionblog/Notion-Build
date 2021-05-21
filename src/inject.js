060493e4f0b763c3282dd82e5bf88a6914761d3f
yudax42 <yudax42@gmail.com>
2021-05-21 10:36:02 +0100

    add deploy button style

--- src/inject.js
+++ src/inject.js
@@ -7,13 +7,23 @@ chrome.storage.sync.get(['link'], function (result) {
     buttonDiv.setAttribute("id", "trigger_build")
     buttonDiv.innerHTML = `
             <iframe width="0" height="0" border="0" name="frm" id="frm" style="display: none;"></iframe>
-            <form action="${result.link}" target="frm" method="post">
-                <button style="background:blue; color:white" type="submit">Deploy</button>
+            <form style="" action="${result.link}" target="frm" method="post">
+                <button style="color: white;
+                line-height: 1.2;
+                background: rgb(0, 150, 109);
+                padding-left: 8px;
+                padding-right: 8px;
+                font-size: 14px;
+                font-weight: 500;
+                height: 26px;
+                border: none;
+                margin-right: 12px;
+                border-radius: 2px;" type="submit">Deploy</button>
             </form>`
     const observer = new MutationObserver(function () {
-      let toolbar = document.getElementsByClassName('notion-page-controls')[0]
+      let toolbar = document.getElementsByClassName('notion-collection-view-item-add')[0]
       let button = document.getElementById("trigger_build")
-      if (toolbar && !button) toolbar.appendChild(buttonDiv)
+      if (toolbar && !button) toolbar.prepend(buttonDiv)
     });
     const observerConfig = {
       attributes: true,
